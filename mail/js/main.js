e7fe5bc19f79d44ce190727113405c4a3afda3da
Onda-dev <manuelaltieri3@gmail.com>
2022-05-03 16:05:30 +0200

    Esito controllo ok

--- mail/js/main.js
+++ mail/js/main.js
@@ -3,11 +3,11 @@ const emailUtente = prompt("Inserisci il tuo indirizzo email")
 // controlla che sia nella lista di chi può accedere
 // stampa un messaggio appropriato sull’esito del controllo
 const listaEmail = ["manuel@manuel.it", "ok@ok.it", "altieri@altieri.it", "boolean@ok.it", "email@email.it"]
-// console.log(listaEmail)
-for(let i = 0; i < listaEmail.lenght; i++) {
-    if (emailUtente == listaEmail) {
-        alert ('yes');
-    } else {
-        alert ('no');
+let permesso = "Accesso rifiutato";
+for(let i = 0; i < listaEmail.length; i++) {
+    if (emailUtente == listaEmail[i]) {
+        permesso = "Accesso approvato"
     }
 }
+
+alert (permesso)
